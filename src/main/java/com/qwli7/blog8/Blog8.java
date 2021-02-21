35a8b3b244037bc4497a5b3e032bba741ef6ba2c
qwli7 <228794754@qq.com>
2021-02-21 23:56:01 +0800

    initial project

---
+++ src/main/java/com/qwli7/blog8/Blog8.java
@@ -0,0 +1,12 @@
+package com.qwli7.blog8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blog8 {
+
+    public static void main(String[] args){
+        SpringApplication.run(Blog8.class, args);
+    }
+}
