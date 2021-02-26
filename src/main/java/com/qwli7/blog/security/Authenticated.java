3d26579f183eed8fe06c6c6df487ff1e4c3739d4
Q <228794754@qq.com>
2021-02-26 22:35:58 +0800

    Update Authenticated.java

--- src/main/java/com/qwli7/blog/security/Authenticated.java
+++ src/main/java/com/qwli7/blog/security/Authenticated.java
@@ -1,4 +1,13 @@
 package com.qwli7.blog.security;
 
+import java.lang.annotation.*;
+
+/**
+ * @author qwli7
+ */
+@Inherited
+@Documented
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Authenticated {
 }
