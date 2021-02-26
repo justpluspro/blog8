a50fb462efba505934af230efe3ee9e1f407a55d
Q <228794754@qq.com>
2021-02-26 22:38:35 +0800

    Create BlogConfig.java

---
+++ src/main/java/com/qwli7/blog/entity/BlogConfig.java
@@ -0,0 +1,74 @@
+package com.qwli7.blog.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author qwli7
+ * @date 2021/2/26 13:55
+ * 功能：blog
+ **/
+public class BlogConfig implements Serializable {
+
+    public BlogConfig() {
+        super();
+    }
+
+    public BlogConfig(BlogConfig blogConfig) {
+        super();
+        this.password = blogConfig.getPassword();
+        this.loginName = blogConfig.getLoginName();
+        this.email = blogConfig.getEmail();
+        this.nickname = blogConfig.getNickname();
+        this.avatar = blogConfig.getAvatar();
+    }
+
+    private String avatar;
+
+    private String loginName;
+
+    private String nickname;
+
+    private String email;
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
