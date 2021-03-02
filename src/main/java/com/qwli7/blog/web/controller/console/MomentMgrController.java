b00d1c52a8f30a70cbf9333e44251e8c8853159a
Q <228794754@qq.com>
2021-03-02 20:40:53 +0800

    Update MomentMgrController.java

--- src/main/java/com/qwli7/blog/web/controller/console/MomentMgrController.java
+++ src/main/java/com/qwli7/blog/web/controller/console/MomentMgrController.java
@@ -1,16 +1,33 @@
 package com.qwli7.blog.web.controller.console;
 
+import com.qwli7.blog.entity.Moment;
+import com.qwli7.blog.service.MomentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("console")
 @Controller
 public class MomentMgrController {
 
+    private final MomentService momentService;
+
+    public MomentMgrController(MomentService momentService) {
+        this.momentService = momentService;
+    }
+
     @GetMapping("moments")
     public String index() {
         return "console/moment/index";
+    }
+
+    @GetMapping("moment/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+        Moment moment = momentService.getMomentForEdit(id);
+        model.addAttribute(moment);
+        return "console/moment/edit";
     }
 
 }
