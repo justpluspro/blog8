ad32aa4311d05a097299680b210d418551ddcbba
Q <228794754@qq.com>
2021-03-02 20:37:02 +0800

    Create MomentQueryParam.java

---
+++ src/main/java/com/qwli7/blog/entity/vo/MomentQueryParam.java
@@ -0,0 +1,18 @@
+package com.qwli7.blog.entity.vo;
+
+/**
+ * @author qwli7 
+ * @date 2021/3/2 10:17
+ * 功能：blog
+ **/
+public class MomentQueryParam extends AbstractQueryParam {
+    private String query;
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
