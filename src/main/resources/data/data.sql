5e6e48c80bcbaf3ae90a7ec4135cafcc7bbfbaaf
Q <228794754@qq.com>
2021-03-02 20:42:44 +0800

    Create data.sql

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,45 @@
+insert into blog_category(name) values ('java');
+insert into blog_category(name) values ('life');
+insert into blog_category(name) values ('web');
+
+/*init blog_tag data*/
+insert into blog_tag(name) values ('android');
+insert into blog_tag(name) values ('nodejs');
+insert into blog_tag(name) values ('netty');
+insert into blog_tag(name) values ('ios');
+insert into blog_tag(name) values ('ipp');
+insert into blog_tag(name) values ('线程池');
+insert into blog_tag(name) values ('maven');
+insert into blog_tag(name) values ('pom');
+insert into blog_tag(name) values ('distribute');
+insert into blog_tag(name) values ('repository');
+insert into blog_tag(name) values ('thymeleaf');
+insert into blog_tag(name) values ('element-ui');
+insert into blog_tag(name) values ('iview');
+insert into blog_tag(name) values ('json');
+insert into blog_tag(name) values ('jackson');
+insert into blog_tag(name) values ('fastjson');
+insert into blog_tag(name) values ('gson');
+insert into blog_tag(name) values ('google');
+insert into blog_tag(name) values ('chrome');
+insert into blog_tag(name) values ('firefox');
+insert into blog_tag(name) values ('ajax');
+insert into blog_tag(name) values ('jquery');
+insert into blog_tag(name) values ('markdown');
+insert into blog_tag(name) values ('csdn');
+insert into blog_tag(name) values ('jna');
+insert into blog_tag(name) values ('electron');
+insert into blog_tag(name) values ('jwt');
+insert into blog_tag(name) values ('aes');
+insert into blog_tag(name) values ('logback');
+insert into blog_tag(name) values ('log4j');
+insert into blog_tag(name) values ('image');
+insert into blog_tag(name) values ('video');
+insert into blog_tag(name) values ('audio');
+
+/*init blog_moment*/
+insert into blog_moment(content, hits, comments, is_private, allow_comment) values('测试动态', 0, 0, false, true);
+insert into blog_moment(content, hits, comments, is_private, allow_comment) values('测试动态2', 0, 0, false, true);
+insert into blog_moment(content, hits, comments, is_private, allow_comment) values('测试动3', 0, 0, false, true);
+insert into blog_moment(content, hits, comments, is_private, allow_comment) values('测试动态4', 0, 0, false, true);
+insert into blog_moment(content, hits, comments, is_private, allow_comment) values('测试动态5', 0, 0, false, true);
