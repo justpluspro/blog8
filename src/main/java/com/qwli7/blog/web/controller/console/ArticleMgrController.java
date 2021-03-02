c293fe0d0dee5f6b844ad781f566d68d0f8a1350
Q <228794754@qq.com>
2021-03-02 20:40:40 +0800

    Update ArticleMgrController.java

--- src/main/java/com/qwli7/blog/web/controller/console/ArticleMgrController.java
+++ src/main/java/com/qwli7/blog/web/controller/console/ArticleMgrController.java
@@ -1,7 +1,10 @@
 package com.qwli7.blog.web.controller.console;
 
+import com.qwli7.blog.service.ArticleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("console")
@@ -9,9 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ArticleMgrController {
 
 
+    private final ArticleService articleService;
+
+    public ArticleMgrController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
     @GetMapping("articles")
     public String index() {
         return "console/article/index";
+    }
+
+    @GetMapping("article/{id}/edit")
+    public String edit(@PathVariable("id") int id, Model model) {
+
+        return "console/article/edit";
     }
 
 }
