95035735e2ef33e4e6dedc09360cbbb2e33101b7
Q <228794754@qq.com>
2021-03-02 20:38:39 +0800

    Create FileUtil.java

---
+++ src/main/java/com/qwli7/blog/file/FileUtil.java
@@ -0,0 +1,19 @@
+package com.qwli7.blog.file;
+
+
+/**
+ * @author qwli7
+ * @date 2021/3/2 8:54
+ * 功能：blog
+ **/
+public class FileUtil {
+    private FileUtil() {
+        super();
+    }
+
+
+    public static String getFileExtension(String fileName) {
+
+        return "";
+    }
+}
