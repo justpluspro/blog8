14a220836493df8a8a382db3214c255e3e8f4840
Q <228794754@qq.com>
2021-03-08 18:19:21 +0800

    Update HandledArticleQueryParam.java

--- src/main/java/com/qwli7/blog/entity/vo/HandledArticleQueryParam.java
+++ src/main/java/com/qwli7/blog/entity/vo/HandledArticleQueryParam.java
@@ -1,6 +1,9 @@
 package com.qwli7.blog.entity.vo;
 
+import com.qwli7.blog.entity.ArticleStatus;
 import com.qwli7.blog.entity.Category;
+
+import java.util.List;
 
 public class HandledArticleQueryParam extends AbstractQueryParam {
 
@@ -8,6 +11,25 @@ public class HandledArticleQueryParam extends AbstractQueryParam {
 
     private Category category;
 
+    private List<Integer> aids;
+
+    private List<ArticleStatus> statuses;
+
+    public List<ArticleStatus> getStatuses() {
+        return statuses;
+    }
+
+    public void setStatuses(List<ArticleStatus> statuses) {
+        this.statuses = statuses;
+    }
+
+    public List<Integer> getAids() {
+        return aids;
+    }
+
+    public void setAids(List<Integer> aids) {
+        this.aids = aids;
+    }
 
     public String getQuery() {
         return query;
@@ -25,7 +47,14 @@ public class HandledArticleQueryParam extends AbstractQueryParam {
         this.category = category;
     }
 
-    public HandledArticleQueryParam(ArticleQueryParam queryParam) {
+    public HandledArticleQueryParam() {
+        super();
+    }
 
+    public HandledArticleQueryParam(ArticleQueryParam queryParam) {
+        this.setPage(queryParam.getPage());
+        this.setSize(queryParam.getSize());
+        this.setOffset(queryParam.getOffset());
+        this.setStart(queryParam.getStart());
     }
 }
