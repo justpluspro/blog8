3cb489b0c394dc6063940be86056bd2d032384ad
Q <228794754@qq.com>
2021-03-16 19:14:47 +0800

    Create JsoupUtil.java

---
+++ src/main/java/com/qwli7/blog/util/JsoupUtil.java
@@ -0,0 +1,32 @@
+package com.qwli7.blog.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * @author qwli7
+ * @date 2021/3/16 13:46
+ * 功能：blog
+ **/
+public class JsoupUtil {
+
+    private JsoupUtil() {
+        super();
+    }
+
+    public static Optional<String> getFirstImage(String html) {
+        if(StringUtils.isEmpty(html)) {
+            return Optional.empty();
+        }
+
+        Element img = Jsoup.parse(html).selectFirst("img");
+        if(img == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(img.attr("src"));
+    }
+}
