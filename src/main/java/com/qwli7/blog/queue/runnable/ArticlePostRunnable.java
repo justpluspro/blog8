d4752aea9ca0efe8d9b5da349496b56fcb1549aa
qwli7 <228794754@qq.com>
2021-04-22 23:28:34 +0800

    optimize imports

--- src/main/java/com/qwli7/blog/queue/runnable/ArticlePostRunnable.java
+++ src/main/java/com/qwli7/blog/queue/runnable/ArticlePostRunnable.java
@@ -2,8 +2,16 @@ package com.qwli7.blog.queue.runnable;
 
 import com.qwli7.blog.entity.Article;
 
+/**
+ * 文章发布线程
+ * @author liqiwen
+ * @since 2.1
+ */
 public class ArticlePostRunnable implements Runnable {
 
+    /**
+     * 待发布的文章
+     */
     private final Article article;
 
     public Article getArticle() {
@@ -16,6 +24,9 @@ public class ArticlePostRunnable implements Runnable {
 
     @Override
     public void run() {
+        // 变更文章的状态
+
+        // 插入索引
 
     }
 }
