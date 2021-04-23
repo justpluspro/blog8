ed2410c6192c1e5ec93bd59223fdb8353ec41d6a
qwli7 <228794754@qq.com>
2021-04-23 22:56:17 +0800

    add ImageConvertParam

---
+++ src/main/java/com/qwli7/blog/file/vo/ImageConvertParam.java
@@ -0,0 +1,11 @@
+package com.qwli7.blog.file.vo;
+
+import java.io.Serializable;
+
+/**
+ * 图片转换参数
+ * @author liqiwen
+ * @since 2.1
+ */
+public class ImageConvertParam implements Serializable {
+}
