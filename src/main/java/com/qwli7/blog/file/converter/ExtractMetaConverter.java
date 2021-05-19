230936f197b444c2710c2e2ffb77d8edcd55f967
qwli7 <228794754@qq.com>
2021-05-20 00:36:35 +0800

    ffprobe converter

--- src/main/java/com/qwli7/blog/file/converter/ExtractMetaConverter.java
+++ src/main/java/com/qwli7/blog/file/converter/ExtractMetaConverter.java
@@ -68,6 +68,155 @@ public class ExtractMetaConverter extends AbstractMediaConverter {
     }
 
 
+    /**
+     * 利用 ffprobe 获取数据原信息
+     * ffprobe -v quiet -print_format json -show_format -show_streams /Users/liqiwen/Desktop/video.mp4
+     * ffprobe -loglevel quiet -show_format -show_streams /Users/liqiwen/Desktop/video.mp4 -print_format json
+     * @param sourceFile sourceFile
+     * @return commands
+     */
+    private List<String> createMetaInfoCommandUseFfprobe(File sourceFile) {
+        List<String> commands = new ArrayList<>();
+        commands.add("-loglevel");
+        commands.add("quiet");
+        commands.add("-show_format");
+        commands.add("-show_streams");
+        commands.add(sourceFile.getAbsolutePath());
+        commands.add("-print_format");
+        commands.add("json");
+
+        return commands;
+    }
+
+
+    /**
+     * 提取元数据信息使用 ffprobe
+     * @param sourceFile sourceFile
+     *       {
+     *     "streams": [
+     *         {
+     *             "index": 0,
+     *             "codec_name": "h264",
+     *             "codec_long_name": "H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10",
+     *             "profile": "High",
+     *             "codec_type": "video",
+     *             "codec_time_base": "1/50",
+     *             "codec_tag_string": "avc1",
+     *             "codec_tag": "0x31637661",
+     *             "width": 1280,
+     *             "height": 720,
+     *             "coded_width": 1280,
+     *             "coded_height": 720,
+     *             "closed_captions": 0,
+     *             "has_b_frames": 2,
+     *             "pix_fmt": "yuv420p",
+     *             "level": 50,
+     *             "chroma_location": "left",
+     *             "refs": 1,
+     *             "is_avc": "true",
+     *             "nal_length_size": "4",
+     *             "r_frame_rate": "25/1",
+     *             "avg_frame_rate": "25/1",
+     *             "time_base": "1/12800",
+     *             "start_pts": 0,
+     *             "start_time": "0.000000",
+     *             "duration_ts": 539136,
+     *             "duration": "42.120000",
+     *             "bit_rate": "1384716",
+     *             "bits_per_raw_sample": "8",
+     *             "nb_frames": "1053",
+     *             "disposition": {
+     *                 "default": 1,
+     *                 "dub": 0,
+     *                 "original": 0,
+     *                 "comment": 0,
+     *                 "lyrics": 0,
+     *                 "karaoke": 0,
+     *                 "forced": 0,
+     *                 "hearing_impaired": 0,
+     *                 "visual_impaired": 0,
+     *                 "clean_effects": 0,
+     *                 "attached_pic": 0,
+     *                 "timed_thumbnails": 0
+     *             },
+     *             "tags": {
+     *                 "language": "und",
+     *                 "handler_name": "VideoHandler"
+     *             }
+     *         },
+     *         {
+     *             "index": 1,
+     *             "codec_name": "aac",
+     *             "codec_long_name": "AAC (Advanced Audio Coding)",
+     *             "profile": "LC",
+     *             "codec_type": "audio",
+     *             "codec_time_base": "1/44100",
+     *             "codec_tag_string": "mp4a",
+     *             "codec_tag": "0x6134706d",
+     *             "sample_fmt": "fltp",
+     *             "sample_rate": "44100",
+     *             "channels": 2,
+     *             "channel_layout": "stereo",
+     *             "bits_per_sample": 0,
+     *             "r_frame_rate": "0/0",
+     *             "avg_frame_rate": "0/0",
+     *             "time_base": "1/44100",
+     *             "start_pts": 0,
+     *             "start_time": "0.000000",
+     *             "duration_ts": 1852421,
+     *             "duration": "42.005011",
+     *             "bit_rate": "129085",
+     *             "max_bit_rate": "129085",
+     *             "nb_frames": "1810",
+     *             "disposition": {
+     *                 "default": 1,
+     *                 "dub": 0,
+     *                 "original": 0,
+     *                 "comment": 0,
+     *                 "lyrics": 0,
+     *                 "karaoke": 0,
+     *                 "forced": 0,
+     *                 "hearing_impaired": 0,
+     *                 "visual_impaired": 0,
+     *                 "clean_effects": 0,
+     *                 "attached_pic": 0,
+     *                 "timed_thumbnails": 0
+     *             },
+     *             "tags": {
+     *                 "language": "eng",
+     *                 "handler_name": "SoundHandler"
+     *             }
+     *         }
+     *     ],
+     *     "format": {
+     *         "filename": "/Users/liqiwen/Desktop/video.mp4",
+     *         "nb_streams": 2,
+     *         "nb_programs": 0,
+     *         "format_name": "mov,mp4,m4a,3gp,3g2,mj2",
+     *         "format_long_name": "QuickTime / MOV",
+     *         "start_time": "0.000000",
+     *         "duration": "42.120000",
+     *         "size": "8005082",
+     *         "bit_rate": "1520433",
+     *         "probe_score": 100,
+     *         "tags": {
+     *             "major_brand": "isom",
+     *             "minor_version": "512",
+     *             "compatible_brands": "isomiso2avc1mp41",
+     *             "encoder": "Lavf58.51.101"
+     *         }
+     *     }
+     * }
+     */
+    private void extractMetaInfoUseFfprobe(File sourceFile) {
+        final List<String> commands = createMetaInfoCommandUseFfprobe(sourceFile);
+        final String resultStr = doProcess(commands, getFfprobePath());
+        logger.info("resultStr: [{}]", resultStr);
+
+    }
+
+
+
     private void extractVideoMetaInfo(File sourceFile) {
 
         final List<String> commands = buildVideoMetaInfoCommand(sourceFile);
